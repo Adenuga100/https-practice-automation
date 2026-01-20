befaef6fe4348e0ef1e3cb89f09471bac714f28a
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-01-20 02:54:04 -0800

    javascript delay

--- tests/learnqa.spec.ts
+++ tests/learnqa.spec.ts
@@ -1,9 +1,11 @@
 import test from "@playwright/test";
 import { HomePage } from "../pages/Home.po";
+import { JavascriptDelayPage } from "../pages/Javascriptdelay.po";
 
 test("JavaScript Delays", async ({ page }) => {
     await page.goto("/");
-
     await HomePage.clickNameButtonsBytext(page, "JavaScript Delays");
+    await JavascriptDelayPage.clickJavascriptDelayButton(page);
+    await JavascriptDelayPage.getTittle(page).isVisible({ timeout: 20000 });
 
 });
